ea041711539443263807b6d7db6047482a0d2693
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2024-03-18 00:36:12 -0400

    init

---
+++ source/problem3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Instructions: Complete the code below by replacing the to-do's with working code. (10 pts)
+
+##########################################################################
+
+
+def cube_root_approximation(number, tolerance=1e-6):
+    # TODO 3. Find and fix the error to complete the function.
+    # Initial guess for the cube root
+    # guess = number / 2.0 # one way to start
+    guess = 5
+    # Iterate until the approximation is within the specified tolerance
+    while abs(guess**3 - number) > tolerance:
+        # Update the guess using the approximation formula
+        guess = (2 * guess + number / (guess**2)) / 3.0
+        print(f" guess = {guess}")
+
+# end of cube_root_approximation
+
+# Example: Calculate the cube root of 29791
+input_number = 29791
+
+# TODO: 1. Calculate the approximation by using the above function.
+    # result = ...
+# TODO: 2. Print the approximated value.
+    # print(...)
